f9e5533d641b827e16a38766cfdf6caadfc0c468
Bruno Pocay <brunopocay@gmail.com>
2023-07-13 23:28:08 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/SuperHeroController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI.Data;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public SuperHeroController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task <ActionResult<List<SuperHero>>> GetSuperHeroes()
+        {
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task <ActionResult<List<SuperHero>>> CreateSuperHero(SuperHero hero)
+        {
+            _context.SuperHeroes.Add(hero);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task <ActionResult<List<SuperHero>>> UpdateSuperHero(SuperHero hero)
+        {
+            var dbhero = await _context.SuperHeroes.FindAsync(hero.Id);
+            if (hero == null)
+            {
+                return BadRequest("Hero Not Found");
+            }
+
+            dbhero.Name = hero.Name;
+            dbhero.firstName = hero.firstName;
+            dbhero.lastName = hero.lastName;
+            dbhero.place = hero.place;
+
+            await _context.SaveChangesAsync();
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]     
+        public async Task <ActionResult<List<SuperHero>>> DeleteSuperHero(int Id)
+        {
+            var dbhero = await _context.SuperHeroes.FindAsync(Id);
+            if ( dbhero == null )
+            {
+                return BadRequest("Hero Not Found");
+            }
+
+            _context.SuperHeroes.Remove(dbhero);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        static readonly string connectionString = "Server=localhost; User ID=root; Password=Pokas@131293; Database=SuperHeroNG;";
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+
+        public DbSet<SuperHero> SuperHeroes => Set<SuperHero>();
+    }
+}
